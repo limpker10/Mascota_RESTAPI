9717b79ada601164c7375d5b751a8c3fd9adbeab
Jose Cano <jcanov@unsa.edu.pe>
2023-09-06 08:23:39 -0500

    Configuración básica del proyecto de CRUD de Mascotas RestAPI"

---
+++ src/main/java/pe/edu/tecsup/mascotasapi/MascotasApiRestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.mascotasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotasApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MascotasApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/tecsup/mascotasapi/MascotasApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.mascotasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
